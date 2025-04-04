218286ed0d63a74052015e41fcf6bce52cf32b8c
writermct <morganmuat@gmail.com>
2025-04-03 21:43:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example: Smooth scroll for anchor links
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
